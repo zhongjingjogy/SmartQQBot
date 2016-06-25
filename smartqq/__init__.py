1f7f02f278a58fa07d06a9b2bebace5210402310
zhongjingjogy <zhongjingjogy@gmail.com>
2016-06-25 09:11:44 +0800

    add decorated filters for the messages.

--- smartqq/__init__.py
+++ smartqq/__init__.py
@@ -2,7 +2,9 @@ from simpleapp import start_qq
 from plugin_signals import (
     on_all_message,
     on_group_message,
-    on_private_message
+    on_private_message,
+    on_from_uin_message,
+    message_filter
 )
 from list_message import list_messages
 from model.create import create_db
--- smartqq/plugin_signals.py
+++ smartqq/plugin_signals.py
@@ -4,28 +4,32 @@ from bot.messages import (
     PRIVATE_MSG
 )
 
-def default_handle(msg, bot, handler):
+def default_handle(*args, **kwargs):
     # print("process message with the default handler")
     pass
 
-"""
-message_decorate : set a filter for a plugin;
-function_decorate : the decorator.
-wrap_func : wrap the decorated function according to the message type.
-"""
-def message_decorate(msg_type):
+def message_filter(key, value=None):
     def function_decorate(func):
-        def wrap_func(msg, bot, handler):
-            if msg_type == "all":
-                return func(msg, bot, handler)
-            elif msg.type == msg_type:
-                return func(msg, bot, handler)
+        def wrap_func(*args, **kwargs):
+            if value == getattr(kwargs["msg"], key):
+                return func(*args, **kwargs)
             else:
-                return default_handle(msg, bot, handler)
+                return default_handle(*args, **kwargs)
         return wrap_func
     return function_decorate
 
-# wrap the message decorator into different filters.
-on_all_message = message_decorate("all")
-on_group_message = message_decorate(GROUP_MSG)
-on_private_message = message_decorate(PRIVATE_MSG)
+on_group_message = message_filter("msg_type", GROUP_MSG)
+on_private_message = message_filter("msg_type", PRIVATE_MSG)
+
+def on_all_message(func):
+    def wrap_func(*args, **kwargs):
+        return func(*args, **kwargs)
+    return wrap_func
+
+def on_from_uin_message(value):
+    def function_decorate(func):
+        @message_filter("from_uin", value=value)
+        def wrap_func(*args, **kwargs):
+            return func(*args, **kwargs)
+        return wrap_func
+    return function_decorate
