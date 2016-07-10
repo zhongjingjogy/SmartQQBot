50d85e8f31febba7d6b732f8d5cfcd481f371dd3
zhongjingjogy <zhongjingjogy@gmail.com>
2016-07-10 19:33:16 +0800

    update the model for recording the messages.

--- smartqq/model/model.py
+++ smartqq/model/model.py
@@ -6,17 +6,6 @@ from datetime import datetime
 
 Base = declarative_base()
 
-"""
-poll_type : message
-    from_uin : 2895548428
-    msg_id : 48558
-    msg_type : 0
-    to_uin : 260700286
-    content :
-    _content : [[u'font', {u'color': u'000000', u'style': [0, 0, 0], u'name': u'\u5fae\u8f6f\u96c5\u9ed1', u'size': 10}], [u'face', 5]]
-    time : 2016-06-23 18:46:38
-    font : {u'color': u'000000', u'style': [0, 0, 0], u'name': u'\u5fae\u8f6f\u96c5\u9ed1', u'size': 10}
-"""
 
 class Message(Base):
     __tablename__ = "message"
@@ -39,6 +28,7 @@ class Message(Base):
 class PrivateMessage(Message):
     __tablename__ = "privatemessage"
     id = Column(Integer, ForeignKey('message.id'), primary_key=True)
+    nickname = Column(String(100))
     def __str__(self):
         items = ["id", "poll_type", "from_uin", "msg_id", "msg_type", "to_uin", "content", "time"]
         values = [self.id, self.poll_type, self.from_uin, self.msg_id, self.msg_type, self.to_uin, self.content,  self.recievedtime]
@@ -67,5 +57,22 @@ class SaturoRecord(Base):
     def __str__(self):
         return "\{{key}: {value}\}".format(key=self.key, value=self.value)
 
+    def __unicode__(self):
+        return unicode(self.__str__())
+
+class Friend(Base):
+    __tablename__ = "friend"
+    id = Column(Integer, primary_key=True)
+    account = Column(Integer)
+    nickname = Column(String(100))
+    birthday = Column(DateTime, default=datetime.utcnow)
+    gender = Column(String(20))
+    province = Column(String(20))
+    city = Column(String(20))
+    uin = Column(Integer)
+    def __str__(self):
+        items = ["account", "nickname", "birthday", "gender", "province", "city", "uin"]
+        items = [self.account, self.nickname, self.birthday, self.gender, self.province, self.city, self.uin]
+        return "Friend : \n\t" + "\n\t".join(["%s : %s" % (k, v) for k, v in zip(items, values)])
     def __unicode__(self):
         return unicode(self.__str__())
